ba9c1d88ff7dac19f3aca7e4306a9e0041040c5c
Drew Burgess <87342933+DrewTheGiraffe@users.noreply.github.com>
2023-11-02 16:43:43 -0500

    jellyfin backup script
    initial commit

---
+++ backup-jellyfin.ps1
@@ -0,0 +1,18 @@
+﻿$date = Get-Date -Format "MM-dd-yyyy"
+$backupname = "JellyFinServer_Backup-${date}"
+
+New-Item -Path "C:\temp" -Name $backupname -ItemType Directory -Force  
+
+If (Test-Path -Path "C:\Program Files\Jellyfin") {
+    Copy-Item -Path "C:\Program Files\Jellyfin" -Destination "C:\temp\$backupname\JellyfinProgramFiles" -Verbose -Recurse -Force
+}
+
+If (Test-Path -Path "C:\ProgramData\Jellyfin") {
+    Copy-Item -Path "C:\ProgramData\Jellyfin" -Destination "C:\temp\$backupname\JellyfinProgramData" -Verbose -Recurse -Force
+}
+
+Write-Host "Creating backup [" -NoNewline
+Write-Host "${backupname}.zip" -NoNewline -ForegroundColor Green
+Write-Host "] to C:\temp"
+
+Compress-Archive -Path "C:\temp\$backupname" -DestinationPath "C:\temp\$backupname.zip" -Verbose -Force
