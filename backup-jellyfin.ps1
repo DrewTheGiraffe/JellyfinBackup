1e265277bef122fa303ab340d868e5f085a7b93a
Drew Burgess <87342933+DrewTheGiraffe@users.noreply.github.com>
2023-11-19 14:59:57 -0600

    Update backup-jellyfin.ps1

--- backup-jellyfin.ps1
+++ backup-jellyfin.ps1
@@ -3,7 +3,14 @@
 $date = Get-Date -Format "MM-dd-yyyy"
 $backupname = "JellyFinServer_Backup-${date}"
 
-New-Item -Path "C:\temp" -Name $backupname -ItemType Directory -Force  
+If (Test-Path "C:\temp") {
+    New-Item -Path "C:\temp" -Name $backupname -ItemType Directory -Force  
+}
+else {
+    New-Item -Path "C:\" -Name "temp" -ItemType Directory -Force
+    Start-Sleep -Milliseconds 200
+    New-Item -Path "C:\temp" -Name $backupname -ItemType Directory -Force  
+}
 
 If (Test-Path -Path "C:\Program Files\Jellyfin") {
     Copy-Item -Path "C:\Program Files\Jellyfin" -Destination "C:\temp\$backupname\JellyfinProgramFiles" -Verbose -Recurse -Force
@@ -13,11 +20,14 @@ If (Test-Path -Path "C:\ProgramData\Jellyfin") {
     Copy-Item -Path "C:\ProgramData\Jellyfin" -Destination "C:\temp\$backupname\JellyfinProgramData" -Verbose -Recurse -Force
 }
 
+Clear-Host
+
 Write-Host "Creating backup [" -NoNewline
 Write-Host "${backupname}.zip" -NoNewline -ForegroundColor Green
 Write-Host "] to C:\temp"
 
+Start-Sleep -Seconds 2
+
 Compress-Archive -Path "C:\temp\$backupname" -DestinationPath "C:\temp\$backupname.zip" -Verbose -Force
 
-# Remove copied data after compression
-Remove-Item -Path "C:\temp\$backupname" -Recurse -Force
+Remove-Item -Path "C:\temp\JellyFinServer_Backup-${date}" -Recurse -Force
