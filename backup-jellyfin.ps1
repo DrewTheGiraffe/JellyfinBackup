83fc155f83f55dc9fa84fd68595a67de89fd8661
Drew Burgess <87342933+DrewTheGiraffe@users.noreply.github.com>
2024-01-26 20:20:16 -0600

    Update backup-jellyfin.ps1
    Last little update for awhile, just removed some hard coded paths that don't really need to be set for public usage.

--- backup-jellyfin.ps1
+++ backup-jellyfin.ps1
@@ -1,35 +1,57 @@
-# Simple Jellyfin server backup script for windows.. | By Drew Burgess 2023.
+# Simple Jellyfin server backup script for windows.. 
+# Source: https://github.com/DrewTheGiraffe/JellyfinBackup/edit/main/backup-jellyfin.ps1
 
 $date = Get-Date -Format "MM-dd-yyyy"
 $backupname = "JellyFinServer_Backup-${date}"
+$public_backup_path = "${env:USERPROFILE}\Desktop"
 
-If (Test-Path "C:\temp") {
-    New-Item -Path "C:\temp" -Name $backupname -ItemType Directory -Force  
+# Stop server before backing up to avoid file corruption
+Get-Process | Where {$_.ProcessName -like "*jellyfin*"} | Stop-Process -Force
+
+# Check for process existance before running backup...
+$verify = (Get-Process | Where {$_.ProcessName -like "*jellyfin*"} | select $_)
+If ($null -eq $verify) {
+
+    $DestinationPath = "$public_backup_path\$backupname"
+
+    If (Test-Path $DestinationPath -eq $false) {
+        New-Item -Path "${env:USERPROFILE}\Desktop" -Name $backupname -ItemType Directory -Force  
+    }
+    
+
+    If (Test-Path -Path "C:\Program Files\Jellyfin") {
+        Copy-Item -Path "C:\Program Files\Jellyfin" -Destination "$DestinationPath\JellyfinProgramFiles" -Verbose -Recurse -Force
+    }
+
+    If (Test-Path -Path "C:\ProgramData\Jellyfin") {
+        Copy-Item -Path "C:\ProgramData\Jellyfin" -Destination "$DestinationPath\JellyfinProgramData" -Verbose -Recurse -Force
+    }
+
+    If (Test-Path -Path "C:\Users\user1\Documents\mediashare") {
+        Copy-Item -Path "C:\Users\user1\Documents\mediashare" -Destination "$DestinationPath\customimages" -Verbose -Recurse -Force
+
+        Clear-Host
+
+        Start-Sleep -Seconds 2
+
+    }
+
+    Compress-Archive -Path "$public_backup_path\$backupname" -DestinationPath "$public_backup_path\$backupname.zip" -Verbose -Force
+
+    Remove-Item -Path "$public_backup_path\$backupname" -Recurse -Force
+
+    Start-Sleep -Milliseconds 100
+
+    Clear-Host
+
+    Write-Host "Created backup [" -NoNewline
+    Write-Host "${backupname}.zip" -NoNewline -ForegroundColor Green
+    Write-Host "] to $public_backup_path"
+
+    write-Host "`nYou will need to start jellyfin manually." -ForegroundColor Green
+
 }
-else {
-    New-Item -Path "C:\" -Name "temp" -ItemType Directory -Force
-    Start-Sleep -Milliseconds 200
-    New-Item -Path "C:\temp" -Name $backupname -ItemType Directory -Force  
+# start server when backup is finished
+If (Test-Path -Path "C:\Program Files\Jellyfin\Server") {
+    Start-Process -FilePath "C:\Program Files\Jellyfin\Server" -ArgumentList ".\jellyfin.exe"
 }
-
-If (Test-Path -Path "C:\Program Files\Jellyfin") {
-    Copy-Item -Path "C:\Program Files\Jellyfin" -Destination "C:\temp\$backupname\JellyfinProgramFiles" -Verbose -Recurse -Force
-}
-
-If (Test-Path -Path "C:\ProgramData\Jellyfin") {
-    Copy-Item -Path "C:\ProgramData\Jellyfin" -Destination "C:\temp\$backupname\JellyfinProgramData" -Verbose -Recurse -Force
-}
-
-Clear-Host
-
-Write-Host "Creating backup [" -NoNewline
-Write-Host "${backupname}.zip" -NoNewline -ForegroundColor Green
-Write-Host "] to C:\temp"
-
-Start-Sleep -Seconds 2
-
-Compress-Archive -Path "C:\temp\$backupname" -DestinationPath "C:\temp\$backupname.zip" -Verbose -Force
-
-Remove-Item -Path "C:\temp\JellyFinServer_Backup-${date}" -Recurse -Force
-
-start C:\temp
